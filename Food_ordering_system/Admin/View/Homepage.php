edc8749fb30004346ecb488d610ec828e906d6f9
Azmain Adib <azmain7461@gmail.com>
2026-01-04 12:15:22 +0600

    2

---
+++ Food_ordering_system/Admin/View/Homepage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Add Food</title>
+    </head>
+    <body>
+        
+    <h2>Add Food</h2>
+    <form action="">
+        Food Name: <input type="text" name="name"><br><br>
+        Price: <input type="text" name="price"><br><br>
+        
+    </form>
+    </body>
+</html>
