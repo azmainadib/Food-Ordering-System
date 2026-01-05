dbc74a0a2284ff661856a56102120b9d4763741f
Azmain Adib <azmain7461@gmail.com>
2026-01-05 09:35:36 +0600

    5

--- Food_ordering_system/Admin/View/dashboard.php
+++ Food_ordering_system/Admin/View/dashboard.php
@@ -11,6 +11,17 @@
         <a href="addfood.php">Add Food </a>
         <a href="addvoucher.php">Add Voucher </a>
         <a href="viewvouchar.php">View Vouchar </a>
+
+        <table border="1">
+            <tr><th>Image</th><th>Name</th><th>Price</th><th>Delete</th></tr>
+            <?php while($f=mysqli_fetch_assoc($res)){ ?>
+                <tr>
+                    <td><img src="../Upload/<?= $f['image']?>" width="65"></td>
+                    <td><?= $f['name'] ?></td>
+                    <td><?= $f['price'] ?></td>
+                </tr>
+            }
+        </table>
     </form>
 </body>
 </html>
