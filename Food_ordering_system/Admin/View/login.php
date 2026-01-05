4ad003a3b7e7ff0ea80ebe5a54241a5779756f11
Azmain Adib <azmain7461@gmail.com>
2026-01-05 10:18:06 +0600

    10

--- Food_ordering_system/Admin/View/login.php
+++ Food_ordering_system/Admin/View/login.php
@@ -8,7 +8,13 @@
             <h1>User Login</h1>
             <label for="">Email ID:</label>
             <input type="text" placeholder="Enter Email ID"id="in">
-            
+            <label for="">Password</label>
+            <input type"password" placeholder="Enter Password" id="in">
+
+            <a href=""id="fp">Forgot Password?</a>
+            <input type="submit"value="Login"id="sub">
+            <a href="../View/userreg.php"id="reg">Create Account</a>
+
 
         </form>
     </body>
