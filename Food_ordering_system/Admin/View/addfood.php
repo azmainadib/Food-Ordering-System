6b4c97ccb40288913f0a0597f649f3bd0d66b5db
Azmain Adib <azmain7461@gmail.com>
2026-01-09 03:49:36 +0600

    18

--- Food_ordering_system/Admin/View/addfood.php
+++ Food_ordering_system/Admin/View/addfood.php
@@ -1,12 +1,8 @@
-<?php
-include "../Control/addfood.php";
-?>
-
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Add Food</title>
+        <link rel="stylesheet" href="../CSS/add food.css">
     </head>
     <body>
         
