99407a83381ff52fe06d9cd0cb49ae2bb9a53029
Azmain Adib <azmain7461@gmail.com>
2026-01-19 20:57:10 +0600

    36

--- Food_ordering_system/Admin/View/dashboard.php
+++ Food_ordering_system/Admin/View/dashboard.php
@@ -9,5 +9,7 @@
             <a href="adminlogin.php" id="btn 1">Sign in</a>
             <a href="adminreg.php" id="btn 2">Sign up</a>
         </header>
+        <h2>Welcome</h2>
+        <footer>Food Ordering System</footer>
     </body>
 </html>
