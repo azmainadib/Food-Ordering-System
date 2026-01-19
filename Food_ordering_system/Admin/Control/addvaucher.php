9d925bba5c1c63675cbccc260c8669d3c73c0f4d
Azmain Adib <azmain7461@gmail.com>
2026-01-19 15:49:19 +0600

    21

--- Food_ordering_system/Admin/Control/addvaucher.php
+++ Food_ordering_system/Admin/Control/addvaucher.php
@@ -13,4 +13,15 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
             {
                 $error="All fields required";
             }
+        else
+            {$sql="INSERT INTO voucher(code,discount)VALUES('$code','$discount')";
+        if($conn->query($sql))
+            {
+                $success="Vaucher added successfully.";
+            }
+        else{
+            $error="Error: " . $conn->error;
+        }
+
+    }
     }
