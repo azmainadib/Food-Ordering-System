066b1b1b6cc334a0d420daed618d62cab8bacba3
Azmain Adib <azmain7461@gmail.com>
2026-01-19 16:05:44 +0600

    23

--- Food_ordering_system/Admin/View/addvoucher.php
+++ Food_ordering_system/Admin/View/addvoucher.php
@@ -7,16 +7,20 @@
         <title>Add vaucher</title>
     </head>
     <body>
+        <a href="adminhome.php">Home</a>
+        <a href="addfood.php">Add Food</a>
+        <a href="addvoucher.php">Add Voucher</a>
+        <a href="viewvoucher.php">View Voucher</a>
         
     <form action = "" method="post">
-        <h2>Add vaucher</h2>
+        <h2>Add voucher</h2>
         <label for="">code:</label>
         <input type="text" name="code">
         <label for="">Discount:</label>
         <input type="text" name="discount">
         <button name="add">Generate</button>
-        <p style="color:green;"><?php echo $success;?></p>
-        <p style="color:red;"><?php echo $error;?></p>
+        <p style="color:green;"><?php echo $success; ?></p>
+        <p style="color:red;"><?php echo $error; ?></p>
 
     </form>
     </body>
