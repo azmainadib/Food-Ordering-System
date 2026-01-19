b26c16da3d77b70a5d5136180160b9811f87c57c
Azmain Adib <azmain7461@gmail.com>
2026-01-19 16:37:44 +0600

    27

---
+++ Food_ordering_system/Admin/View/viewfood.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>View food</title>
+</head>
+
+<body>
+    <table border="1">
+        <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Delete</th>
+        </tr>
+        <?php while ($f = mysqli_fetch_assoc($res)) { ?>
+            <tr>
+                <td><img src="../uploads/<?= $f['image'] ?>" width="60"></td>
+                <td><?= $f['name'] ?></td>
+                <td><?= $f['price'] ?></td>
+                <td><a href="../controller/deletefood.php?id=<?= $f['id'] ?>">Delete</a></td>
+            </tr>
+        <?php } ?>
+</body>
+
+</html>
