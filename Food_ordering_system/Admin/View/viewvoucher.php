fb7fb2a76560435c44d539c503ad41e88e57b15e
Azmain Adib <azmain7461@gmail.com>
2026-01-19 16:59:12 +0600

    33

--- Food_ordering_system/Admin/View/viewvoucher.php
+++ Food_ordering_system/Admin/View/viewvoucher.php
@@ -1,43 +1,48 @@
 <?php
 include "../DB/db.php";
-$res=mysqli_query($conn,"SELECT * FROM voucher");
+$res = mysqli_query($conn, "SELECT * FROM voucher");
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>
-        Vouchers
-    </title>
+    <title>Vouchers</title>
+    <link rel="stylesheet" href="../CSS/viewvoucher.css">
+</head>
 
 <body>
-    <form action="">
-        <a href="adminhome.php">Home</a>
-        <a href="addfood.php">Add Food</a>
-        <a href="addvoucher.php">Add Voucher</a>
-        <a href="viewvoucher.php">View Voucher</a>
-        <a href="viewfood.php">viewfood</a>
-    </form>
-    <form action="">
-        <h2>Voucher List</h2>
-        <table border="1">
-            <tr>
-                <th>Code</th>
-                <th>Discount</th>
 
-                <th>Delete</th>
+    <div id="menu">
+        <a href="adminhome.php" class="menu-link">Home</a>
+        <a href="addfood.php" class="menu-link">Add Food</a>
+        <a href="addvoucher.php" class="menu-link">Add Voucher</a>
+        <a href="viewvoucher.php" class="menu-link">View Voucher</a>
+        <a href="viewfood.php" class="menu-link">View Food</a>
+    </div>
+
+    <h2 id="page-title">Voucher List</h2>
+
+    <table id="voucher-table">
+        <tr>
+            <th>Code</th>
+            <th>Discount</th>
+            <th>Delete</th>
+        </tr>
+
+        <?php while ($v = mysqli_fetch_assoc($res)) { ?>
+            <tr>
+                <td><?= $v['code'] ?></td>
+                <td><?= $v['discount'] ?>%</td>
+                <td>
+                    <a class="delete-link" href="../controller/deletevoucher.php?id=<?= $v['id'] ?>">
+                        Delete
+                    </a>
+                </td>
             </tr>
-            <?php while ($v = mysqli_fetch_assoc($res)) { ?>
-                <tr>
-                    <td><?= $v['code'] ?></td>
-                    <td><?= $v['discount'] ?>%</td>
-                    <td><a href="../controller/deletevoucher.php ?id=<?= $v['id'] ?>">Delete</a></td>
-                </tr>
-            <?php } ?>
-        </table>
-    </form>
+        <?php } ?>
+    </table>
+
 </body>
-</head>
 
 </html>
