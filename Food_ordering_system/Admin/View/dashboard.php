eed39a35b01ac839ea55a29894feedfc2a10b2d7
Azmain Adib <azmain7461@gmail.com>
2026-01-19 21:16:59 +0600

    38

--- Food_ordering_system/Admin/View/dashboard.php
+++ Food_ordering_system/Admin/View/dashboard.php
@@ -2,12 +2,13 @@
 <html>
     <head>
         <title>Food Ordering System</title>
+        <link rel="stylesheet" href="../CSS/dashboard.css">
     </head>
     <body>
         <header>
             <h1>Food Ordering System</h1>
-            <a href="adminlogin.php" id="btn 1">Sign in</a>
-            <a href="adminreg.php" id="btn 2">Sign up</a>
+            <a href="login.php" id="btn 1">Sign in</a>
+            <a href="registration.php" id="btn 2">Sign up</a>
         </header>
         <h2>Welcome</h2>
         <footer>Food Ordering System</footer>
